1da5e15f18891aa617a8830d83c4e953f747bce1
Vexa69 <xav2109@gmail.com>
2021-07-26 11:02:45 +0200

    update

--- Backend/controllers/authCtrl.js
+++ Backend/controllers/authCtrl.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
 	if (!req.body.userName || !req.body.email || !req.body.password) {
-		return res.status(400).json({ message: 'one more paramaters empty' });
+		return res.status(400).json({ message: 'one or more paramaters empty' });
 	}
 	const nameRegex = /(.*[a-z]){3,30}/;
 	const mailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
